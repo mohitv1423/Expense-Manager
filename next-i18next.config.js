47f4ad6cc3acbaa7a993660ee23db3a16da38770
Mohit verma <mohitv1423@gmail.com>
2026-06-29 09:40:08 +0530

    code pushed

---
+++ next-i18next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next-i18next').UserConfig} */
+const config = {
+  i18n: {
+    defaultLocale: 'default',
+    locales: [
+      'default',
+      'en',
+      'de',
+      'fr',
+      'it',
+      'cs',
+      'nl',
+      'pl',
+      'pt-PT',
+      'pt-BR',
+      'sv',
+      'es',
+      'es-MX',
+      'es-AR',
+      'hu',
+    ],
+    localeDetection: false,
+  },
+  localePath: './public/locales',
+};
+
+export default config;
